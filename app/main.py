7dc418d23e7382f01f72b7ec94977a71bad0baed
Oleksandr <aleksandrpaskoo22@gmail.com>
2025-03-09 15:32:39 +0100

    Implement

--- app/main.py
+++ app/main.py
@@ -1,3 +1,30 @@
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+        self.file = None
+
+    def __enter__(self):
+        if not os.path.exists(self.filename):
+            open(self.filename, "w").close()
+
+        self.file = open(self.filename, "r")
+        return self
+
+    def read(self) -> str:
+        if self.file:
+            return self.file.read()
+        return ""
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        if self.file:
+            self.file.close()
+
+    def __del__(self) -> None:
+        try:
+            if os.path.exists(self.filename):
+                os.remove(self.filename)
+        except Exception:
+            pass
